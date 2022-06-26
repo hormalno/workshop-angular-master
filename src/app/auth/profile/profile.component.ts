623f2c79ec82f80b6779ea88691276fbf69d5dff
hormalno <ivanov.yasen@gmail.com>
2022-06-26 17:08:06 +0300

    asd

--- src/app/auth/profile/profile.component.ts
+++ src/app/auth/profile/profile.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { IUser } from 'src/app/core/interfaces';
 import { UserService } from 'src/app/core/user.service';
+import { IAuthModuleState } from '../+store';
+import { enterEditMode, exitEditMode, profilePageInitalized, updateProfileStarted } from '../+store/actions';
+
+const bgCountryTelPrefix = '00359';
 
 @Component({
   selector: 'app-profile',
@@ -13,44 +19,82 @@ export class ProfileComponent implements OnInit {
 
   @ViewChild('editProfileForm') editProfileForm!: NgForm;
 
-  currentUser!: IUser;
+  currentUser$: Observable<IUser> = this.store.select(state => state.auth.profile.currentProfile);
 
-  isInEditMode: boolean = false;
+  isInEditMode$: Observable<boolean> = this.store.select(state => state.auth.profile.isInEditMode);
 
-  constructor(private userService: UserService, private router: Router) { }
+  hasErrorHappened: Observable<boolean> = this.store.select(state => state.auth.profile.errorHappened);
+
+  newProfilePicture?: File;
+
+  constructor(private userService: UserService,
+    private router: Router,
+    private store: Store<IAuthModuleState>) { }
 
   ngOnInit(): void {
-    this.userService.getProfile$().subscribe({
-      next: (user) => {
-        this.currentUser = user;
-      },
-      error: () => {
-        this.router.navigate(['/login'])
+    this.store.dispatch(profilePageInitalized());
+
+    this.hasErrorHappened.subscribe((hasError) => {
+      if (hasError) {
+        this.router.navigate(['/user/login'])
       }
     })
+    // this.userService.getProfile$().subscribe({
+    //   next: (user) => {
+    //     this.store.dispatch(profileLoaded({ profile: user }));
+    //   },
+    //   error: () => {
+    //   this.router.navigate(['/user/login'])
+    //   }
+    // })
   }
 
-  enterEditMode(): void {
-    this.isInEditMode = true;
+  enterEditMode(currentUser: IUser): void {
+    this.store.dispatch(enterEditMode());
 
     setTimeout(() => {
       this.editProfileForm.form.patchValue({
-        email: this.currentUser.email,
-        username: this.currentUser.username,
-        'select-tel': this.currentUser.tel && this.currentUser.tel.length > 4
-          ? this.currentUser.tel.substring(0, 4) : '',
-        tel: this.currentUser.tel && this.currentUser.tel.length > 4
-          ? this.currentUser.tel.substring(4) :
-          this.currentUser.tel
+        email: currentUser.email,
+        username: currentUser.username,
+        // The below 2 properties were updated offline.
+        'select-tel': (currentUser.tel || '').startsWith(bgCountryTelPrefix) ? bgCountryTelPrefix : '',
+        tel: (currentUser.tel || '').startsWith(bgCountryTelPrefix) ? currentUser.tel.replace(bgCountryTelPrefix, '') : currentUser.tel,
+        // End of offline edit.
       })
     });
   }
 
   updateProfile(): void {
-    // TODO stoimenovg: continue.
-    console.log(this.editProfileForm.value);
+    // Offline updated code.
+    let countryPrefix = this.editProfileForm.value['select-tel'];
+    if (!this.editProfileForm.value.tel) {
+      countryPrefix = '';
+    }
 
-    this.isInEditMode = false;
+    this.store.dispatch(updateProfileStarted({
+      user: {
+        username: this.editProfileForm.value.username,
+        email: this.editProfileForm.value.email,
+        tel: countryPrefix + this.editProfileForm.value.tel,
+        profilePicture: this.newProfilePicture,
+      }
+    }));
+    // End of offline update.
+
+    // Previous code.
+    // // TODO stoimenovg: continue.
+    // console.log(this.editProfileForm.value);
+
+    // this.exitEditMode();
+  }
+
+  exitEditMode(): void {
+    this.store.dispatch(exitEditMode());
+  }
+
+  handleProfilePictureChange(event: InputEvent) {
+    const input: HTMLInputElement = event.target as HTMLInputElement;
+    this.newProfilePicture = input.files![0];
   }
 
 }
